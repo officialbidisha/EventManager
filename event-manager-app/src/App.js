dcf61c07d9cd7ca48313575ceb8b50c871b36cc7
Bidisha Das <officialbidisha1@gmail.com>
2024-02-11 12:46:02 +0530

    Feat: added two columns

--- event-manager-app/src/App.js
+++ event-manager-app/src/App.js
@@ -4,11 +4,13 @@ import EventList from "./components/EventList";
 function App() {
   return (
     <div className="App">
-      <div className="row">
+      <div className="row border">
         <div className="flex-column">
+          <p className="event-header">All Events</p>
           <EventList />
         </div>
         <div className="flex-column">
+        <p className="event-header">Selected Events</p>
         <EventList />
         </div>
       </div>
