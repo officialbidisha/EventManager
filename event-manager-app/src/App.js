1ce30a49040ca09d465d74486ef937ef07e2c3f5
Bidisha Das <officialbidisha1@gmail.com>
2024-02-11 12:33:48 +0530

    Feat:Style fix

--- event-manager-app/src/App.js
+++ event-manager-app/src/App.js
@@ -1,15 +1,16 @@
-
-import './App.css';
-import Events from './components/Events';
-import EventList from './components/EventList';
+import "./App.css";
+import Events from "./components/Events";
+import EventList from "./components/EventList";
 function App() {
   return (
     <div className="App">
-      <div className='flex-column'>
-        <EventList/>
-      </div>
-      <div className=''>
-        
+      <div className="row">
+        <div className="flex-column">
+          <EventList />
+        </div>
+        <div className="flex-column">
+        <EventList />
+        </div>
       </div>
     </div>
   );
